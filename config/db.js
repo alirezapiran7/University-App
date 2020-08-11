377a59a3664ac41d21e1c1ebbef3bd2075fa9c72
Ramon742 <ramondavi-developer@outlook.com>
2020-08-11 10:06:50 -0300

    bug fixed

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+//const config = require('config');
+//const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect( db, {
+        await mongoose.connect( "mongodb://ramon:zxc123@uniufersa-shard-00-00.xlmih.mongodb.net:27017,uniufersa-shard-00-01.xlmih.mongodb.net:27017,uniufersa-shard-00-02.xlmih.mongodb.net:27017/test?ssl=true&replicaSet=atlas-ye3cu4-shard-0&authSource=admin&retryWrites=true&w=majority", {
             useNewUrlParser: true,
 			useCreateIndex: true,
 			useFindAndModify: false,
